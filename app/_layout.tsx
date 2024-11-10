0e61890e62f746c2cad715ac87aa32bb4649d314
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-10 13:36:53 +0400

    font added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,13 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+  useFonts({
+    'outfit': require('./../assets/fonts/Outfit-Regular.ttf'),
+    'outfitimedium': require('./../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold': require('./../assets/fonts/Outfit-Bold.ttf'),
+  })
+  
   return (
     <Stack>
       <Stack.Screen name="index" />
